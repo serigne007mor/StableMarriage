e8e24691cec534c03bc143afc85cdc16ba5df89c
serigne007mor <serignemortoure1@gmail.com>
2019-11-07 23:34:15 -0600

    refactor

--- src/StableMarriage/StableMarriage.java
+++ src/StableMarriage/StableMarriage.java
@@ -6,18 +6,18 @@ public class StableMarriage {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
-        int prefer[][] = new int[][] { { 7, 5, 6, 4 }, { 5, 4, 6, 7 }, { 4, 5, 6, 7 }, { 4, 5, 6, 7 }, { 0, 1, 2, 3 },
+        int preferenceList[][] = new int[][] { { 7, 5, 6, 4 }, { 5, 4, 6, 7 }, { 4, 5, 6, 7 }, { 4, 5, 6, 7 }, { 0, 1, 2, 3 },
                 { 0, 1, 2, 3 }, { 0, 1, 2, 3 }, { 0, 1, 2, 3 } };
-        stableMarriage(prefer);
+        stableMarriage(preferenceList);
     }
 
-    static boolean wPrefersM1OverM(int preference[][], int currentWomen, int currentMen, int currentAssignment) {
-        for (int i = 0; i < preference[0].length; i++) {
-            if (preference[currentWomen][i] == currentAssignment){
+    static boolean wPrefersM1OverM(int preferenceList[][], int currentWomen, int currentMen, int currentAssignment) {
+        for (int i = 0; i < preferenceList[0].length; i++) {
+            if (preferenceList[currentWomen][i] == currentAssignment){
                 return true;
             }
 
-            if (preference[currentWomen][i] == currentMen){
+            if (preferenceList[currentWomen][i] == currentMen){
                 return false;
             }
 
@@ -25,32 +25,32 @@ public class StableMarriage {
         return false;
     }
 
-    static void stableMarriage(int preference[][]) {
-        int currentWomenProposal[] = new int[preference[0].length];
+    static void stableMarriage(int preferenceList[][]) {
+        int currentWomenProposal[] = new int[preferenceList[0].length];
 
-        boolean freeMen[] = new boolean[preference[0].length];
+        boolean freeMen[] = new boolean[preferenceList[0].length];
 
         Arrays.fill(currentWomenProposal, -1);
-        int freeCount = preference[0].length;
+        int freeCount = preferenceList[0].length;
 
         while (freeCount > 0) {
 
             int currentMen;
-            for (currentMen = 0; currentMen < preference[0].length; currentMen++)
+            for (currentMen = 0; currentMen < preferenceList[0].length; currentMen++)
                 if (freeMen[currentMen] == false){
                     break;
                 }
 
-            for (int i = 0; i < preference[0].length && freeMen[currentMen] == false; i++) {
-                int w = preference[currentMen][i];
-                if (currentWomenProposal[w - preference[0].length] == -1) {
-                    currentWomenProposal[w - preference[0].length] = currentMen;
+            for (int i = 0; i < preferenceList[0].length && freeMen[currentMen] == false; i++) {
+                int w = preferenceList[currentMen][i];
+                if (currentWomenProposal[w - preferenceList[0].length] == -1) {
+                    currentWomenProposal[w - preferenceList[0].length] = currentMen;
                     freeMen[currentMen] = true;
                     freeCount--;
                 } else {
-                    int m1 = currentWomenProposal[w - preference[0].length];
-                    if (wPrefersM1OverM(preference, w, currentMen, m1) == false) {
-                        currentWomenProposal[w - preference[0].length] = currentMen;
+                    int m1 = currentWomenProposal[w - preferenceList[0].length];
+                    if (wPrefersM1OverM(preferenceList, w, currentMen, m1) == false) {
+                        currentWomenProposal[w - preferenceList[0].length] = currentMen;
                         freeMen[currentMen] = true;
                         freeMen[m1] = false;
                     }
@@ -59,9 +59,9 @@ public class StableMarriage {
         }
 
         System.out.println("Woman   Man");
-        for (int i = 0; i < preference[0].length; i++) {
+        for (int i = 0; i < preferenceList[0].length; i++) {
             System.out.print(" ");
-            System.out.println(i + preference[0].length + "	 " + currentWomenProposal[i]);
+            System.out.println(i + preferenceList[0].length + "	 " + currentWomenProposal[i]);
         }
     }
 
